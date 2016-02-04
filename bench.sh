44afb7681be37affd49cd49e6f2aa745e33b6e3a
bobo liu <admin@boboweb.net>
2016-02-05 04:19:40 +0800

    Create bench.sh

---
+++ bench.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+#==============================================================#
+#   Description: bench test script                             #
+#   Author: Teddysun <i@teddysun.com>                          #
+#   Thanks: LookBack <admin@dwhd.org>                          #
+#   Visit:  https://teddysun.com                               #
+#==============================================================#
+# Modified by bobo
+
+get_opsy() {
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+    [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+    [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+}
+
+next() {
+    printf "%-70s\n" "-" | sed 's/\s/-/g'
+}
+
+speed_test() {
+    speedtest=$(wget -4O /dev/null -T300 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
+    ipaddress=$(ping -c1 -n `awk -F'/' '{print $3}' <<< $1` | awk -F'[()]' '{print $2;exit}')
+    nodeName=$2
+    if   [ "${#nodeName}" -lt "8" ]; then
+        echo -e "\e[33m$2\e[0m\t\t\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+    elif [ "${#nodeName}" -lt "13" ]; then
+        echo -e "\e[33m$2\e[0m\t\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+    elif [ "${#nodeName}" -lt "24" ]; then
+        echo -e "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+    elif [ "${#nodeName}" -ge "24" ]; then
+        echo -e "\e[33m$2\e[0m\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+    fi
+}
+
+speed_test_v6() {
+    speedtest=$(wget -6O /dev/null -T300 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
+    ipaddress=$(ping6 -c1 -n `awk -F'/' '{print $3}' <<< $1` | awk -F'[()]' '{print $2;exit}')
+    nodeName=$2
+    if   [ "${#nodeName}" -lt "8" -a "${#ipaddress}" -eq "13" ]; then
+        echo -e "\e[33m$2\e[0m\t\t\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+    elif [ "${#nodeName}" -lt "13" -a "${#ipaddress}" -eq "13" ]; then
+        echo -e "\e[33m$2\e[0m\t\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+    elif [ "${#nodeName}" -lt "24" -a "${#ipaddress}" -eq "13" ]; then
+        echo -e "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+    elif [ "${#nodeName}" -lt "24" -a "${#ipaddress}" -gt "13" ]; then
+        echo -e "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t\e[31m$speedtest\e[0m"
+    elif [ "${#nodeName}" -ge "24" ]; then
+        echo -e "\e[33m$2\e[0m\t\e[32m$ipaddress\e[0m\t\e[31m$speedtest\e[0m"
+    fi
+}
+
+speed_test_local() {
+    wget http://sh.bobiji.com/localtest.py
+    python localtest.py 
+    localspeed=$(awk '{print 98304/$1}' .localtest)
+    echo -e "Your local-to-server speed is \e[32m$localspeed\e[0m KB/S."
+}
+speed() {
+    speed_test 'http://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.32.zip' 'Google'
+    speed_test 'http://mirrors.sohu.com/debian-cd/8.2.0/arm64/iso-cd/debian-8.2.0-arm64-netinst.iso' 'Sohu'
+    speed_test 'http://mirrors.tuna.tsinghua.edu.cn/archlinuxarm/os/GoFlexRescue.zip' 'Tsinghua(Education network)'
+}
+
+speed_v6() {
+    speed_test_v6 'http://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.32.zip' 'Google'
+    speed_test_v6 'http://mirrors.tuna.tsinghua.edu.cn/archlinuxarm/os/GoFlexRescue.zip' 'Tsinghua(Education network)'
+}
+
+io_test() {
+    (LANG=en_US dd if=/dev/zero of=botest bs=32k count=3k ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+}
+
+cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//' )
+cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+freq=$( awk -F: '/cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//' )
+tram=$( free -m | awk '/Mem/ {print $2}' )
+swap=$( free -m | awk '/Swap/ {print $2}' )
+up=$( awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60;d=$1%60} {printf("%ddays, %d:%d:%d\n",a,b,c,d)}' /proc/uptime )
+opsy=$( get_opsy )
+arch=$( uname -m )
+lbit=$( getconf LONG_BIT )
+host=$( hostname )
+kern=$( uname -r )
+ipv6=$( wget -qO- -t1 -T2 ipv6.icanhazip.com )
+
+clear
+next
+echo "CPU model            : $cname"
+echo "Number of cores      : $cores"
+echo "CPU frequency        : $freq MHz"
+echo "Total amount of ram  : $tram MB"
+echo "Total amount of swap : $swap MB"
+echo "System uptime        : $up"
+echo "OS                   : $opsy"
+echo "Arch                 : $arch ($lbit Bit)"
+echo "Kernel               : $kern"
+next
+
+if  [ -e '/usr/bin/wget' ]; then
+    echo -e "Node Name\t\t\tIPv4 address\t\tDownload Speed"
+    speed && next
+    if [[ "$ipv6" != "" ]]; then
+        echo -e "Node Name\t\t\tIPv6 address\t\tDownload Speed"
+        speed_v6 && next
+    fi
+else
+    echo "Error: wget command not found. You must be install wget command at first."
+    exit 1
+fi
+
+io1=$( io_test )
+echo "I/O speed : $io1"
+next
+echo "Do you want to test local speed to server?"
+read -p "Y or N:" local
+if [[  $local = 'Y' ]]; then 
+    speed_test_local && next
+else next
+fi
+echo ""
+
+rm -f botest localtest.py .localtest
