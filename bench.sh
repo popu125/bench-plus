d74e923954e39602a8b0d489d8ade1d30fbb8dd5
bobo liu <admin@boboweb.net>
2016-02-05 17:05:24 +0800

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -18,35 +18,38 @@ next() {
 }
 
 speed_test() {
-    speedtest=$(wget -4O /dev/null -T300 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
-    ipaddress=$(ping -c1 -n `awk -F'/' '{print $3}' <<< $1` | awk -F'[()]' '{print $2;exit}')
     nodeName=$2
+    ipaddress=$(ping -c1 -n `awk -F'/' '{print $3}' <<< $1` | awk -F'[()]' '{print $2;exit}') 
     if   [ "${#nodeName}" -lt "8" ]; then
-        echo -e "\e[33m$2\e[0m\t\t\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\t\t\t\e[32m$ipaddress\e[0m\t\t"
     elif [ "${#nodeName}" -lt "13" ]; then
-        echo -e "\e[33m$2\e[0m\t\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\t\t\e[32m$ipaddress\e[0m\t\t"
     elif [ "${#nodeName}" -lt "24" ]; then
-        echo -e "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t\t"
     elif [ "${#nodeName}" -ge "24" ]; then
-        echo -e "\e[33m$2\e[0m\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\e[32m$ipaddress\e[0m\t\t"
     fi
+    speedtest=$(wget -4O /dev/null -T300 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
+    echo -e "\e[0m\t\t\e[31m$speedtest\e[0m"
 }
 
 speed_test_v6() {
-    speedtest=$(wget -6O /dev/null -T300 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
     ipaddress=$(ping6 -c1 -n `awk -F'/' '{print $3}' <<< $1` | awk -F'[()]' '{print $2;exit}')
     nodeName=$2
     if   [ "${#nodeName}" -lt "8" -a "${#ipaddress}" -eq "13" ]; then
-        echo -e "\e[33m$2\e[0m\t\t\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\t\t\t\e[32m$ipaddress\e[0m\t\t"
     elif [ "${#nodeName}" -lt "13" -a "${#ipaddress}" -eq "13" ]; then
-        echo -e "\e[33m$2\e[0m\t\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\t\t\e[32m$ipaddress\e[0m\t\t"
     elif [ "${#nodeName}" -lt "24" -a "${#ipaddress}" -eq "13" ]; then
-        echo -e "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t\t"
     elif [ "${#nodeName}" -lt "24" -a "${#ipaddress}" -gt "13" ]; then
-        echo -e "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\t\e[32m$ipaddress\e[0m\t"
     elif [ "${#nodeName}" -ge "24" ]; then
-        echo -e "\e[33m$2\e[0m\t\e[32m$ipaddress\e[0m\t\e[31m$speedtest\e[0m"
+        echo -e -n "\e[33m$2\e[0m\t\e[32m$ipaddress\e[0m\t\e[31m$speedtest\e[0m"
     fi
+    speedtest=$(wget -6O /dev/null -T300 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
+    echo -e "\e[0m\t\t\e[31m$speedtest\e[0m"
+
 }
 
 speed_test_local() {
@@ -113,11 +116,7 @@ fi
 io1=$( io_test )
 echo "I/O speed : $io1"
 next
-echo "Do you want to test local speed to server?"
-read -p "Y or N:" local
-if [[  $local = 'Y' ]]; then 
-    speed_test_local && next
-fi
+speed_test_local && next
 echo ""
 
 rm -f botest localtest.py .localtest
